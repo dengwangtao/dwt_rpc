61e12187cff86ea626df1f9479cd4bd5e83356a4
dengwangtao <dengwangtao@126.com>
2024-04-06 14:22:19 +0000

    20240406 22:22:17 ne23

--- src/include/rpcprovider.hpp
+++ src/include/rpcprovider.hpp
@@ -6,6 +6,8 @@
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/InetAddress.h>
 #include <muduo/net/TcpConnection.h>
+#include <functional>
+#include <string>
 
 
 // 发布服务类
@@ -22,7 +24,23 @@ private:
 
     void onMessage(const muduo::net::TcpConnectionPtr&, muduo::net::Buffer*,muduo::Timestamp);
 
+    // Closure的回调
+    void sendRpcResponse(const muduo::net::TcpConnectionPtr&, google::protobuf::Message*);
+
+    class MyClosure: public google::protobuf::Closure {
+    public:
+        void Run() override;
+    };
+
 private:
     // 事件循环
     muduo::net::EventLoop m_eventloop;
+
+    // 服务类型信息
+    struct ServiceInfo {
+        google::protobuf::Service* m_service; // 服务对象
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor*> m_methodMap; // 服务方法
+    };
+
+    std::unordered_map<std::string, ServiceInfo> m_serviceMap; // 注册成功的服务对象和服务方法的all信息
 };
--- src/rpcprovider.cpp
+++ src/rpcprovider.cpp
@@ -1,9 +1,35 @@
 #include "rpcprovider.hpp"
 #include "dwtrpcapplication.hpp"
-#include <functional>
+#include "rpcheader.pb.h"
+
+#include <google/protobuf/descriptor.h>
+#include <muduo/base/Logging.h>
 
 void RpcProvider::NotifyService(::google::protobuf::Service *service) {
 
+    // 发布远程调用方法. 反射
+
+    ServiceInfo service_info;
+
+    const google::protobuf::ServiceDescriptor* desc = service->GetDescriptor();
+    // auto desc = service->GetDescriptor();
+
+    std::string service_name = desc->name(); // 获取服务名
+
+    int method_cnt = desc->method_count(); // 获取方法数量
+
+    // std::cout << "\t" << service_name;
+
+    for(int i = 0; i < method_cnt; ++ i) {
+        const google::protobuf::MethodDescriptor* m_desc = desc->method(i);
+        std::string method_name = m_desc->name();
+        service_info.m_methodMap[method_name] = m_desc;
+
+        // std::cout << "\t" << method_name;
+    }
+
+    service_info.m_service = service;
+    m_serviceMap[service_name] = service_info;
 }
 
 
@@ -38,10 +64,95 @@ void RpcProvider::Run() {
 
 
 void RpcProvider::onConnection(const muduo::net::TcpConnectionPtr& conn) {
-
+    if(!conn->connected()) {
+        conn->shutdown(); // 断开连接
+    }
 }
 
-void RpcProvider::onMessage(const muduo::net::TcpConnectionPtr& conn,
-        muduo::net::Buffer* buffer,muduo::Timestamp time) {
+void RpcProvider::onMessage(
+    const muduo::net::TcpConnectionPtr& conn,
+    muduo::net::Buffer* buffer,muduo::Timestamp time
+) {
 
+    std::string buf = buffer->retrieveAllAsString(); // 拿到所有数据
+    // 前四个字节表示 header的长度
+
+    u_int32_t header_size = 0;
+    buf.copy(reinterpret_cast<char*>(&header_size), 4, 0);
+
+    // 头部信息
+    dwt::RpcHeader rpcheader;
+    std::string service_name; // 服务名
+    std::string method_name;  // 方法名
+    int args_size; // 参数长度
+
+    std::string args_str; // 调用方法的参数
+
+    if( rpcheader.ParseFromString(buf.substr(4, header_size)) ) {// 从第4字节开始, 拷贝头部
+        service_name = rpcheader.service_name();
+        method_name = rpcheader.method_name();
+        args_size = rpcheader.args_size();
+
+        args_str = buf.substr(4 + header_size, args_size);
+
+    } else {
+        // 头部格式化失败
+        LOG_ERROR << "header format error";
+        return;
+    }
+
+    LOG_INFO << "=========================";
+    LOG_INFO << "header_size: "  << header_size;
+    LOG_INFO << "service_name: "  << service_name;
+    LOG_INFO << "method_name: "  << method_name;
+    LOG_INFO << "args_size: "  << args_size;
+    LOG_INFO << "header_size: "  << header_size;
+    LOG_INFO << "=========================";
+
+    
+    if(!m_serviceMap.count(service_name)) {
+        LOG_ERROR << "service: " << service_name  << "not found";
+        return;
+    }
+    ServiceInfo& service_info = m_serviceMap[service_name];
+    google::protobuf::Service* service = service_info.m_service; // 获取service对象
+
+    if(!service_info.m_methodMap.count(method_name)) {
+        LOG_ERROR << "[" << service_name << "] method: " << method_name << "not found";
+        return;
+    }
+
+    const google::protobuf::MethodDescriptor* method = service_info.m_methodMap[method_name]; // 获取method对象
+
+    google::protobuf::Message* request = service->GetRequestPrototype(method).New(); // 获取request对象
+    google::protobuf::Message* response = service->GetResponsePrototype(method).New(); // 获取response对象
+
+
+    if(request->ParseFromString(args_str)) {
+        LOG_ERROR << "request parse error. content: " << args_str;
+        return;
+    }
+
+    auto done = 
+        google::protobuf::NewCallback
+            <RpcProvider, const muduo::net::TcpConnectionPtr&, google::protobuf::Message*>
+            (this, &RpcProvider::sendRpcResponse, conn, response);
+
+    service->CallMethod(method, nullptr, request, response, done);
+}
+
+void RpcProvider::sendRpcResponse(const muduo::net::TcpConnectionPtr& conn, google::protobuf::Message* response) {
+    std::string reponse_str;
+
+    if(response->SerializeToString(&reponse_str)) {
+        conn->send(reponse_str);
+    } else {
+        LOG_ERROR << "Serialize Error: " << reponse_str;
+    }
+
+    conn->shutdown();
+}
+
+void RpcProvider::MyClosure::Run() {
+    // nothing
 }
